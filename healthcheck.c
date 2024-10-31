e010a03c526fa3b94e25964d7a53a84d90564267
lucastbmp <lucasmarinari7@gmail.com>
2024-10-30 21:12:13 -0300

    boa noite bruno

--- healthcheck.c
+++ healthcheck.c
@@ -3,7 +3,9 @@
 #include <locale.h>
 
 void my_menu();
-
+void my_ips();
+void add_ip();
+void remove_ip();
 int main() {
 	my_menu();
 	return 0;
@@ -12,9 +14,9 @@ int main() {
 
 void clear_screen() {
 	#ifdef _WIN32
-		system("cls"); /*Para Windows*/
+		system("cls");
 	#else
-		system("clear"); /*Para Linux/MAC*/
+		system("clear");
 	#endif
 
 }
@@ -24,7 +26,7 @@ void my_menu() {
 	int option;
 	clear_screen();
 	printf("-----------------------------------------------\n");
-	printf("\nMyNetwork CLI\n\n");
+	printf("\nHealth Check CLI\n\n");
 	printf("\n 10.0.0.0 - Desconectado.\n");
 	printf("\n1. Ver IPs registrados;");
 	printf("\n2. Adicionar um IP;");
@@ -32,14 +34,46 @@ void my_menu() {
 	printf("\n\n\n\n");
 	printf("----------------------     SD Marinari, 2024. \n");
 	printf("\n");
-	scanf("%d", option);
-
+	scanf("%d", &option);
+	switch(option) {
+		case 1:
+			my_ips();
+			break;
+		case 2:
+			add_ip();
+			break;
+		case 3:
+			remove_ip();
+			break;
+		default:
+			printf("Opção inválida! Tente novamente.\n");
+			break;
+	}	
 }
 
 
 
 void my_ips() {
-	
+	int option;
+	clear_screen();
+	printf("IPs REGISTRADOS: ----------------\n");
+	printf("1. Voltar \n");
+	scanf("%d", &option);	
+	if (option == 1) {
+		my_menu();
+	}
+
 }
 
+
+
+void add_ip() {
+	printf("Função para adicionar IP ainda não implementada.");
+	my_menu();
+}
+
+void remove_ip() {
+	printf("Função para remover um IP ainda não implementada.");
+	my_menu();
+}
 
